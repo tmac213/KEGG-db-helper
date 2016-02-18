b15fdd2c860a38e4895d0067c33f7c601f0b871b
Emilio Colindres <exc231@case.edu>
2016-02-17 21:00:18 -0500

    Refactor into separate packages

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/MainView.fxml"));
 
         primaryStage.setTitle("KEGG Database Helper");
         primaryStage.setScene(new Scene(root, 300, 275));
--- controllers/KEGGController.java
+++ controllers/KEGGController.java
@@ -1,4 +1,4 @@
-package keggdbhelper;
+package keggdbhelper.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
