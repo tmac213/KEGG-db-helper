fd660c26418025a87906c5578b1a5f3bd957c02b
Emilio Colindres <exc231@case.edu>
2016-02-17 22:04:04 -0500

    Refactor for JavaFX Property compliance

--- models/Compound.java
+++ models/Compound.java
@@ -1,16 +1,18 @@
 package keggdbhelper.models;
 
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  * Created by Emilio on 2/17/16.
  */
 public class Compound {
-    String name;
+    private final SimpleStringProperty name;
 
     public Compound(String name) {
-        this.name = name;
+        this.name = new SimpleStringProperty(name);
     }
 
-    public String name() {
-        return this.name;
+    public String getName() {
+        return this.name.get();
     }
 }
