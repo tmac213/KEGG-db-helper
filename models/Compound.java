e96cf75f35434f07d0f965bebd784efa268064aa
Emilio Colindres <exc231@case.edu>
2016-02-19 16:56:59 -0500

    Add ids field to Compound

--- models/Compound.java
+++ models/Compound.java
@@ -1,18 +1,32 @@
 package keggdbhelper.models;
 
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+
+import java.util.List;
 
 /**
  * Created by Emilio on 2/17/16.
  */
 public class Compound {
-    private final SimpleStringProperty name;
+    private SimpleStringProperty name;
+    private SimpleListProperty<String> ids;
 
     public Compound(String name) {
         this.name = new SimpleStringProperty(name);
+        this.ids = null;
     }
 
     public String getName() {
         return this.name.get();
+    }
+
+    public List<String> getids() {
+        return this.ids.get();
+    }
+
+    public void setIds(List<String> ids) {
+        this.ids = new SimpleListProperty<>(FXCollections.observableArrayList(ids));
     }
 }
